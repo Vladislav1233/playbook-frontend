4650e1deae91b82dcc2c68b63bdb5acddf4c89b9
Vladislav Dovzhenko <vladislav.dovjenko@simbirsoft.com>
2018-08-27 17:25:02 +0400

    add calendar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-import ScheduleList from './components/ScheduleList'
+import ScheduleList from './components/ScheduleList';
+import InfiniteCalendar from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css';
 
 class App extends Component {
 
@@ -19,9 +21,23 @@ class App extends Component {
   }
 
   render() {
+    // Render the Calendar
+    const today = new Date();
+    const lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
     // console.log(this.props);
     return (
       <main className="b-main">
+        <InfiniteCalendar 
+          width={400}
+          height={600}
+          selected={today}
+          disabledDates={[0,6]}
+          minDate={lastWeek}
+          displayOptions={{
+            showOverlay: false
+          }}
+        />
+
         {this.renderTemplateListSchedule()}
       </main>
     );
