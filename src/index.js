9e5f0231560eb688a8099cc5c3eb858250369bd6
Roman Bylbas <roman.bylbas@gmail.com>
2018-11-08 14:09:25 +0200

    Switch to hash router

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store/reducers/configureStore';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <Switch>
                 <Route path='/' component={App} />
             </Switch>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>,
     document.getElementById('root')
 );
