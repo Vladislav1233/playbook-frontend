53e684b26c51f1941648472dc27366a6ee3146f0
Roman Bylbas <roman.bylbas@gmail.com>
2018-11-19 15:48:16 +0200

    Added axios package

--- src/pages/TestRequest/TestRequest.js
+++ src/pages/TestRequest/TestRequest.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class TestRequest extends Component {
-    
     registration = (event) => {
         event.preventDefault();
 
-        let payload = {
-            first_name: 'wityasqsi',
-            last_name: 'wityassa',
-            phone: '9178273457',
-            dial_code: "+7",
-            password: '12345678123',
-            c_password: '12345678123',
-            is_trainer: '1'
-        }
-
-        fetch('http://207.154.236.8/api/register',
-            {
-                method: 'POST',
-                body: JSON.stringify(payload)
+        axios({
+            method: 'post',
+            url: 'http://localhost:8001/api/register',
+            data: {
+                first_name: 'wityasqsi',
+                last_name: 'wityassa',
+                phone: '9178273457',
+                dial_code: "+7",
+                password: '12345678123',
+                c_password: '12345678123',
+                is_trainer: '1'
             }
-        )
-    }
+        });
+    };
     
     render() {
         return(
