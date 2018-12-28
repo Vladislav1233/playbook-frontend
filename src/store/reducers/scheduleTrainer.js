954db26868fbecb2bf0050ac70c1069ba98b1f97
vladka223 <vladka.dovzh@gmail.com>
2018-12-28 05:30:30 +0400

    schedule-v2: range date

--- src/store/reducers/scheduleTrainer.js
+++ src/store/reducers/scheduleTrainer.js
@@ -9,73 +9,27 @@ import {
 } from '../constants/schedule';
 
 import moment from 'moment';
+import twix from 'twix';
 
 const initialState = {
     preloader: false,
     scheduleTrainer: {
         date: moment().format('DD.MM.YYYY'),
         nameDay: moment().format('dddd'), 
+        schedule: [{
+            startTime: '12:00',
+            endTime: '14:00',
+            status: 'free'
+        }, {
+            startTime: '14:00',
+            endTime: '15:00',
+            status: 'busy'
+        }],
+        cost: [], // TODO
+        message: [], // TODO, месседж на каких кортах могу с какого времени, его же и выводить в форме букинга
         list: null
     }
 }
-
-// { // Note: Свободное время тренера, но кортов нет
-//     date: '17.12.2018',
-//     nameDay: "Понедельник",
-//     list: [{
-//         idItemScheduleList: 'idItemSchedule4',
-//         startTime: '15:00',
-//         finishTime: '18:00',
-//         status: true,
-//         price: 700,
-//         freeCourt: false,
-//         courts: [{
-//             id: '5',
-//             name: 'Lawn tennis',
-//             street: 'Первомайская',
-//             number: 59,
-//             priority: true
-//         }, {
-//             id: '6',
-//             name: 'Ulgu',
-//             street: 'Московское шоссе',
-//             number: 33,
-//             priority: false
-//         }]
-//     }]
-// }
-
-// { // Note: Тренер занят
-//     date: '19.12.2018',
-//     nameDay: "Среда",
-//     list: [{
-//         id: '8',
-//         idItemScheduleList: 'idItemSchedule6',
-//         startTime: '12:00',
-//         finishTime: '14:00',
-//         status: false
-//     }, {
-//         id: '9',
-//         idItemScheduleList: 'idItemSchedule7',
-//         startTime: '14:00',
-//         finishTime: '18:00',
-//         status: true,
-//         freeCourt: true,
-//         courts: [{
-//             id: '6',
-//             name: 'Lawn tennis',
-//             street: 'Первомайская',
-//             number: 59,
-//             priority: true
-//         }]
-//     }, {
-//         id: '10',
-//         idItemScheduleList: 'idItemSchedule8',
-//         startTime: '18:00',
-//         finishTime: '20:00',
-//         status: false
-//     }]
-// }
 
 export default function(state = initialState, action) {
     switch (action.type) {
@@ -104,6 +58,70 @@ export default function(state = initialState, action) {
             }
 
         case GET_SUCCESS_SCHEDULE_TRAINER:
+            console.log(action.payload.data);
+            const responseSchedule = action.payload.data;
+
+            const imitationBusy = [{ // удалить потом
+                start_time: '2018-12-28 14:00',
+                end_time: '2018-12-28 15:00'
+            }, {
+                start_time: '2018-12-28 17:00',
+                end_time: '2018-12-28 18:00'
+            }]
+
+            let rangeSchedule = [{
+                start: responseSchedule[0].start_time,
+                end: responseSchedule[0].end_time
+            }];
+            console.log(rangeSchedule);
+
+            // Note: получаем дипазоны всего расписания
+            responseSchedule.forEach((item, i, arr) => {
+
+                if(arr.length - 1 !== i) {
+                    if (rangeSchedule[rangeSchedule.length - 1].end === arr[i + 1].start_time) {
+                        rangeSchedule[rangeSchedule.length - 1].end = arr[i + 1].end_time
+                    } else {
+                        rangeSchedule.push({
+                            start: arr[i + 1].start_time,
+                            end: arr[i + 1].end_time
+                        });
+                    };
+                }
+            });
+
+            // Фильтруем диапазон, чтобы в нём не было свободного времени для "занято"
+            imitationBusy.forEach((itemBusy, iBusy, arrBusy) => {
+
+                rangeSchedule.forEach((itemFree, iFree, arrFree) => {
+                    let rangeBusy = moment(itemBusy.start_time).twix(itemBusy.end_time);
+                    let rangeFree = moment(itemFree.start).twix(itemFree.end);
+                    
+                    // Note: если занятое время входит в диапазон
+                    if( rangeFree.engulfs(rangeBusy) ) {
+                        let getRangeWithBusy = rangeFree.xor(rangeBusy);
+                        const newRangeScheduleItem = getRangeWithBusy.map(getRangeWithBusyItem => {
+                            return {
+                                start: getRangeWithBusyItem.start().format('YYYY-MM-DD HH:MM'),
+                                end: getRangeWithBusyItem.end().format('YYYY-MM-DD HH:MM')
+                            }
+                        });
+                        rangeSchedule.splice(iFree, 1, ...newRangeScheduleItem);
+                    }
+                    // console.log(rangeFree.engulfs(rangeBusy));
+                });
+            });
+
+            var range1 = moment("1982-01-23 8:00").twix("1982-01-23 15:00");
+            var range2 = moment("1982-01-23 9:00").twix("1982-01-23 14:00");
+
+            let didi = range1.difference(range2);
+            // console.log(didi[0].format(), didi[1].format());
+
+            // const dataAllSchedule = responseSchedule;
+
+
+
             const newScheduleTrainer = action.payload.data ? action.payload.data.map(item => {
                 return {
                     idItemScheduleList: `idItemSchedule${item.id}`,
