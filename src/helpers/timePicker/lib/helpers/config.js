6ffb03f5170e5d35fcd4f906426258eeafc1b97f
vladka223 <vladka.dovzh@gmail.com>
2019-01-01 17:39:46 +0400

    14-lichnyy-kabinet-trenera: fix elementClockWrapper

---
+++ src/helpers/timePicker/lib/helpers/config.js
@@ -0,0 +1,57 @@
+// eslint-disable-next-line
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/* styles */
+
+// main container
+// eslint-disable-next-line
+var TIMEPICKER_BACKGROUND = exports.TIMEPICKER_BACKGROUND = '#F2F2F2';
+// eslint-disable-next-line
+var FONT_FAMILY = exports.FONT_FAMILY = '"Roboto", sans-serif';
+// eslint-disable-next-line
+var DONE_BUTTON_COLOR = exports.DONE_BUTTON_COLOR = '#686868';
+// eslint-disable-next-line
+var DONE_BUTTON_BORDER_COLOR = exports.DONE_BUTTON_BORDER_COLOR = '#CCC';
+
+// time
+// eslint-disable-next-line
+var TIME_BACKGROUND = exports.TIME_BACKGROUND = 'white';
+// eslint-disable-next-line
+var TIME_DEFAULT_COLOR = exports.TIME_DEFAULT_COLOR = '#8C8C8C';
+// eslint-disable-next-line
+var TIME_SELECTED_COLOR = exports.TIME_SELECTED_COLOR = '#8EDDFD';
+
+// time dropdown
+// eslint-disable-next-line
+var DROPDOWN_BORDER = exports.DROPDOWN_BORDER = '#f4f4f4';
+// eslint-disable-next-line
+var DROPDOWN_COLOR = exports.DROPDOWN_COLOR = '#8c8c8c';
+// eslint-disable-next-line
+var DROPDOWN_SELECTED_COLOR = exports.DROPDOWN_SELECTED_COLOR = '#EAF8FF';
+
+// clock wrapper
+// eslint-disable-next-line
+var CLOCK_WRAPPER_BACKGROUND = exports.CLOCK_WRAPPER_BACKGROUND = '#f4f4f4';
+// eslint-disable-next-line
+var CLOCK_WRAPPER_MERIDIEM_BACKGROUND = exports.CLOCK_WRAPPER_MERIDIEM_BACKGROUND = 'white';
+// eslint-disable-next-line
+var CLOCK_WRAPPER_MERIDIEM_COLOR = exports.CLOCK_WRAPPER_MERIDIEM_COLOR = '#898989';
+// eslint-disable-next-line
+var CLOCK_WRAPPER_MERIDIEM_BACKGROUND_COLOR_SELECTED = exports.CLOCK_WRAPPER_MERIDIEM_BACKGROUND_COLOR_SELECTED = '#E1EFF6';
+// eslint-disable-next-line
+var CLOCK_WRAPPER_MERIDIEM_TEXT_COLOR_SELECTED = exports.CLOCK_WRAPPER_MERIDIEM_TEXT_COLOR_SELECTED = '#898989';
+
+// clock
+// eslint-disable-next-line
+var CLOCK_BACKGROUND = exports.CLOCK_BACKGROUND = 'white';
+// eslint-disable-next-line
+var CLOCK_NUMBER_COLOR = exports.CLOCK_NUMBER_COLOR = '#999999';
+// eslint-disable-next-line
+var CLOCK_HAND_ARM = exports.CLOCK_HAND_ARM = '#bceaff';
+// eslint-disable-next-line
+var CLOCK_HAND_CIRCLE_BACKGROUND = exports.CLOCK_HAND_CIRCLE_BACKGROUND = '#e6f7ff';
+// eslint-disable-next-line
+var CLOCK_HAND_INTERMEDIATE_CIRCLE_BACKGROUND = exports.CLOCK_HAND_INTERMEDIATE_CIRCLE_BACKGROUND = '#ade2fb';
