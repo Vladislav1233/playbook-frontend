375facc5d7f212c7274aee32975c01178ac190ba
vladka223 <vladka.dovzh@gmail.com>
2019-01-20 00:29:00 +0400

    11-o-trenerah: mark list trainer page

--- src/pages/ListTrainer/index.js
+++ src/pages/ListTrainer/index.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { configPathRouter } from '../../App/configPathRouter';
 
+// Note: components
+import ObjectCard from '../../components/ObjectCard';
+
+// Note: styles
+import '../../style/bem-blocks/b-list-trainer/index.scss';
+
 class ListTrainer extends Component {
     render() {
         return (
             <div className="container">
-                <h1>ListTrainer</h1>
-                <Link to={configPathRouter.scheduleTrainer}>Елена Намунка</Link>
+                <div className="b-list-trainer">
+                    <h1>Список тренеров</h1>
+
+                    <ul className="b-list-trainer__list">
+                        <li className="b-list-trainer__item">
+                            <ObjectCard />
+                        </li>
+                    </ul>
+
+                    <Link to={configPathRouter.scheduleTrainer}>Елена Намунка</Link>                    
+                </div>
             </div>
         )
     }
