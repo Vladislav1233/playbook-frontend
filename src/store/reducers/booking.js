a1320185f46cc6b9615aeec09369b841539a96ea
vladka223 <vladka.dovzh@gmail.com>
2019-01-27 16:27:14 +0400

    30-vhodyaschie-zaprosy-na-bron: update state after confirm booking

--- src/store/reducers/booking.js
+++ src/store/reducers/booking.js
@@ -14,10 +14,10 @@ import 'moment/locale/ru';
 import twix from 'twix';
 
 const initialState = {
-    dataBookingRequest: [],
-    dataPastBooking: [],
-    confirmBooking: [],
-    confirmPastBooking: []
+    dataBookingRequest: [], // Note: текущие не подтвержденные заявки
+    dataPastBooking: [], // Note: прошедшие не подтвержденные заявки
+    confirmBooking: [], // Note: текущие подтвержденные заявки
+    confirmPastBooking: [] // Note: прошедшие подтвержденные заявки
 }
 
 export default function(state = initialState, action) {
@@ -35,6 +35,7 @@ export default function(state = initialState, action) {
             
             action.payload.data.data.forEach(item => {
                 if (!moment().isAfter(item.start_time) && !item.status) { // Note: если заявка не истекла и не обработана
+                    console.log(item);
                     newDataBookingRequest.push({
                         firtsName: item.creator.first_name,
                         lastName: item.creator.last_name,
@@ -97,8 +98,31 @@ export default function(state = initialState, action) {
             }
         
         case CONFIRM_BOOKINGS_SUCCESS:
-            return {
-                ...state
+            const confirmId = action.payload.data.data.id;
+            const startTimeConfirm = action.payload.data.data.start_time;
+
+            console.log(action.payload, confirmId);
+
+            if (!moment().isAfter(startTimeConfirm)) { // Note: если заявка не истекла, то удаляем элемент из текущих заявок
+                return {
+                    ...state,
+                    dataBookingRequest: state.dataBookingRequest.filter(itemState => {
+                        console.log(confirmId, itemState.bookingId);
+                        return confirmId !== itemState.bookingId
+                    })
+                }
+            } else if (moment().isAfter(startTimeConfirm)) { // Если заявка истекла, то удаляем её из прошедших заявок
+                return {
+                    ...state,
+                    dataPastBooking: state.dataPastBooking.filter(itemState => {
+                        console.log(confirmId, itemState.bookingId);
+                        return confirmId !== itemState.bookingId
+                    })
+                }
+            } else {
+                return {
+                    ...state
+                }
             }
         
         case CONFIRM_BOOKINGS_FAILURE:
