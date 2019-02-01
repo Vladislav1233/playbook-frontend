ef8ce684494406dfcc1d92ec5d190ad73dbc1a44
vladka223 <vladka.dovzh@gmail.com>
2019-02-01 20:40:47 +0400

    45-fix-add-information-for-trainer: change hashrouter on browserrouter

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store/reducers/configureStore';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-        <HashRouter>
+        <BrowserRouter>
             <Switch>
                 <Route path='/' component={App} />
             </Switch>
-        </HashRouter>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
