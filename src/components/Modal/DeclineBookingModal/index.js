37e948d99cceb030db9ed5c274d01d6634cc93c6
vladka.dovzh@gmail.com <vladka.dovzh@gmail.com>
2019-02-09 14:01:06 +0400

    41-decline-booking: stylization popup for decline booking

--- src/components/Modal/DeclineBookingModal/index.js
+++ src/components/Modal/DeclineBookingModal/index.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 // Note: components
-// import Button from '../../ui-kit/Button/Button';
+import Button from '../../ui-kit/Button/Button';
 import ModalComponent from '../index';
+import Textarea from '../../ui-kit/Textarea';
 
 class DeclineBookingModal extends Component {
 
+	state = {
+		notePlayer: ''
+	};
+
+	handleChangeInput = (event) => {
+        const { name, value } = event.target;
+
+        this.setState({
+            ...this.state,
+            [name]: value
+        })
+    };
+
   	render() {
-    	const { isOpenModal, closeModal } = this.props;
+		const { isOpenModal, closeModal } = this.props;
+		const { notePlayer } = this.state;
 
     	return(
 			<ModalComponent
@@ -15,7 +30,21 @@ class DeclineBookingModal extends Component {
 				closeModal={closeModal}
 				title='Отменить запрос на бронирование'
 			>
+				<Textarea
+					labelText="Сообщение игроку"
+					idInput="notePlayer"
+					nameInput="notePlayer"
+					placeholder="Напишите причину отмены"
+					value={notePlayer}
+					onChange={e => this.handleChangeInput(e)}
+					theme={{blackColor: true}}
+				/>
 
+				<div className="b-modal__button-wrapper">
+					<Button 
+						name="Отказать в бронировании"
+					/>
+				</div>
       		</ModalComponent>
     	)
   	}
