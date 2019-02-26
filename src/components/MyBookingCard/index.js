957f8b72b2a6c17d3dc6041948347055c20b58d8
Amed <amedomary@mail>
2019-02-27 00:27:40 +0400

    добавли часть статус карточки

--- src/components/MyBookingCard/index.js
+++ src/components/MyBookingCard/index.js
@@ -68,8 +68,21 @@ class MyBookingCard extends Component {
             }
         };
 
+        const howStatusClass = (_status) => {
+            switch (_status) {
+                case 0:
+                    return 'wait';
+
+                case 1:
+                    return 'accept';
+
+                case 2:
+                    return 'cancel'
+            }
+        };
+
         return(
-            <div className="b-my-booking-card">
+            <div className={`b-my-booking-card ${howStatusClass(status)}`}>
                 {status !== 2 &&
                     <Fragment>
                         <button onClick={this.openDeclineModal} className="b-close b-close--schedule-item"></button>
@@ -82,33 +95,35 @@ class MyBookingCard extends Component {
                     </Fragment>
                 }
 
-                <div className="b-my-booking-card__type">Забронировано: {typeBookable}</div>
+                <div className="b-my-booking-card__status">
+                    <p className="b-my-booking-card__title">Статус</p>
+                    <div className="b-my-booking-card__status-text">{howStatus(status)}</div>
+                    { status === 2 &&
+                        <div className="b-my-booking-card__status-text">Причина: {note}</div>
+                    }
+                </div>
+
+                <div className="b-my-booking-card__type">
+                    <p className="b-my-booking-card__title">Забронировано</p>
+                    { typeBookable }
+                    { bookableFirstName && ` ${bookableFirstName} ${bookableLastName} ` }
+                </div>
+
                 <div className="b-my-booking-card__time">
+                    <p className="b-my-booking-card__title">Дата и время</p>
                     {moment(startTime).format('DD.MM.YYYY')} ({moment(startTime).format('dddd')})
                     <br/>
                     {moment(startTime).format('HH:mm')} - {moment(endTime).format("HH:mm")}
                 </div>
 
-                <div>
-                    <div>Статус: {howStatus(status)}</div>
-                    {status === 2 && <div>Причина: {note}</div>}
-                </div>
-
-                {bookableFirstName &&
-                    <div className="b-my-booking-card__name-trainer">
-                        {`${bookableFirstName} ${bookableLastName}`}
-                    </div>
-                }
-
                 <div className="b-my-booking-card__playground">
-                    <div className="b-my-booking-card__playground-name">{playgroundName}</div>
-                    <div className="b-my-booking-card__playground-address">{playgroundAddress}</div>
+                    <p className="b-my-booking-card__title">Корт</p>
+                    <div className="b-my-booking-card__playground-name">{playgroundName} ({playgroundAddress})</div>
                 </div>
 
-                <div className="b-cost-information">
-                    <div className="b-cost-information__cost">
-                        {convertTypeMoney(price, 'RUB', 'banknote')} ₽
-                    </div>
+                <div className="b-my-booking-card__cost">
+                    <p className="b-my-booking-card__title">Цена</p>
+                    <div className="b-my-booking-card__cost-text">{convertTypeMoney(price, 'RUB', 'banknote')} ₽</div>
                 </div>
             </div>
         )
