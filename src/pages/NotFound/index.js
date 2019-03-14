04baaf5148f06d16b76e37018a1865bd95c569a2
Max <Amed>
2019-03-14 11:17:42 +0400

    add 404

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 // style
 import '../../style/bem-blocks/b-not-found/index.scss';
 
 class NotFound extends Component {
-    render() {
-        return (
-            <div className="b-not-found">
-                404! Страница не найдена.
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="b-not-found">
+        <div className="container">
+          <h1>
+            404
+          </h1>
+          <div>
+            <h2>Складывается ощущение что вы заблудились...</h2>
+            <p className="b-not-found__description">
+              Давай вернёмся <Link className="link-in-text-white" to="/">на главную</Link> и попробуем ещё раз.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default NotFound; 
+export default NotFound;
