174f945e9908fa5809b77c7b76e71ac17b1c4c70
Max <Amed>
2019-03-23 19:13:55 +0400

    new register

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -26,186 +26,186 @@ const randomBg = bgSrcs[randomIndexBg];
 
 class Registration extends Component {
 
-    handleSubmit = (values) => {
-        const { dispatch } = this.props;
+  handleSubmit = (values) => {
+    const { dispatch } = this.props;
 
-        const userRequestData = {
-            ...values,
-            is_trainer: values.is_trainer ? 1 : 0,
-            phone: telWithoutPlus(values.phone)
-        };
-        dispatch(userActions.register(userRequestData));
-    }
+    const userRequestData = {
+      ...values,
+      is_trainer: values.is_trainer ? 1 : 0,
+      phone: telWithoutPlus(values.phone)
+    };
+    dispatch(userActions.register(userRequestData));
+  }
 
-    render() {
-        const { preloader } = this.props;
+  render() {
+    const { preloader } = this.props;
 
-        return(
-            <div className="b-registration">
-                <img className="b-registration__bg" alt="" src={randomBg} importance="high" />
-                <div className="container">
-                    <div className="b-registration__form-wrapper">
-                        <Form 
-                            onSubmit={this.handleSubmit}
-                            render={({ handleSubmit }) => {
-                                return <form name='register-user' onSubmit={handleSubmit} className="b-registration__form">
-                                    <h1 className="b-registration__heading">Регистрация</h1>
+    return (
+      <div className="b-registration">
+        <img className="b-registration__bg" alt="" src={randomBg} importance="high" />
+        <div className="container">
+          <div className="b-registration__form-wrapper">
+            <Form
+              onSubmit={this.handleSubmit}
+              render={({ handleSubmit }) => {
+                return <form name='register-user' onSubmit={handleSubmit} className="b-registration__form">
+                  <h1 className="b-registration__heading">Регистрация</h1>
 
-                                    <Field 
-                                        name="first_name"
-                                        validate={required()}
-                                        render={({ input, meta }) => {
-                                            return <Input
-                                                {...input}
-                                                nameInput={input.name}
-                                                placeholder="Ваше имя"
-                                                typeInput="text"
-                                                idInput="first_name"
-                                                error={meta.error && meta.touched ? meta.error : null}
-                                            />
-                                        }}
-                                    />
+                  <Field
+                    name="first_name"
+                    validate={required()}
+                    render={({ input, meta }) => {
+                      return <Input
+                        {...input}
+                        nameInput={input.name}
+                        placeholder="Ваше имя"
+                        typeInput="text"
+                        idInput="first_name"
+                        error={meta.error && meta.touched ? meta.error : null}
+                      />
+                    }}
+                  />
 
-                                    <Field 
-                                        name="last_name"
-                                        validate={required()}
-                                        render={({ input, meta }) => {
-                                            return <Input
-                                                {...input}
-                                                placeholder="Ваша фамилия"
-                                                typeInput="text"
-                                                idInput="last_name"
-                                                nameInput={input.name}
-                                                error={meta.error && meta.touched ? meta.error : null}
-                                            />
-                                        }}
-                                    />
-        
-                                    {/*TODO: Сделать маску для номера телефона (для разных стран) */}
-                                    <Field 
-                                        name="phone"
-                                        validate={composeValidators(
-                                            required(),
-                                            fullTelNumber(18) 
-                                        )}
-                                        render={({ input, meta }) => {
-                                            return <div className={
-                                                meta.error && meta.touched
-                                                ? "b-input error"
-                                                : "b-input"}
-                                            >
-                                                <InputMask 
-                                                    className="b-input__input" 
-                                                    id="phone" 
-                                                    mask="+7 (999) 999-99-99" 
-                                                    maskChar={null} 
-                                                    placeholder="Ваш номер телефона"
-                                                    {...input} 
-                                                />
-                                                {meta.error && meta.touched 
-                                                    ? <div className='b-input__error'>{meta.error}</div>
-                                                    :null
-                                                }
-                                        </div> 
-                                        }}
-                                    />
+                  <Field
+                    name="last_name"
+                    validate={required()}
+                    render={({ input, meta }) => {
+                      return <Input
+                        {...input}
+                        placeholder="Ваша фамилия"
+                        typeInput="text"
+                        idInput="last_name"
+                        nameInput={input.name}
+                        error={meta.error && meta.touched ? meta.error : null}
+                      />
+                    }}
+                  />
 
-                                    <Field 
-                                        name="password"
-                                        validate={required()}
-                                        render={({ input, meta }) => {
-                                            return <Input
-                                                {...input}
-                                                placeholder="Пароль"
-                                                typeInput="password"
-                                                idInput="password"
-                                                nameInput={input.name}
-                                                error={meta.error && meta.touched ? meta.error : null}
-                                            /> 
-                                        }}
-                                    /> 
-
-                                    <Field 
-                                        name="c_password"
-                                        validate={(value, allValues) => composeValidators(
-                                            required(),
-                                            confirmPassword(allValues.password)
-                                        )(value)
-                                        }
-                                        render={({ input, meta }) => {
-                                            return <Input
-                                                {...input}
-                                                placeholder="Повторите пароль"
-                                                typeInput="password"
-                                                idInput="c_password"
-                                                nameInput={input.name}
-                                                error={meta.error && meta.touched ? meta.error : null}
-                                            /> 
-                                        }}
-                                    />
-                                    
-                                    {/* { name, id, text, value, checked, modif } */}
-                                    <Field 
-                                        name="is_trainer"
-                                        type='checkbox'
-                                        render={({ input }) => {
-                                            return <Checkbox 
-                                                {...input}
-                                                id="is_trainer"
-                                                value="1"
-                                                modif="b-checkbox--white"
-                                            >
-                                                <span>Я тренер</span>
-                                            </Checkbox>
-                                        }}
-                                    />
-
-                                    <Field 
-                                        name="is_confirm-personal"
-                                        type='checkbox'
-                                        validate={required('Ваше согласие обязательно')}
-                                        render={({ input, meta }) => {
-                                            return <Checkbox
-                                                {...input}
-                                                id="is_confirm-personal"
-                                                modif="b-checkbox--white b-checkbox--align-top"
-                                                error={meta.error && meta.touched ? meta.error : null}
-                                            >
-                                                <span>Я принимаю <a href="/agreement" title="Пользовательское соглашение" target="blank">пользовательское соглашение</a></span>
-                                            </Checkbox>
-                                        }}
-                                    />
-                                    
-                                    <div className="b-registration__button-wrapper">
-                                        <Button
-                                            type='submit'
-                                            modif="b-button--login"
-                                            name='Зарегистрироваться'
-                                        />
-                                    </div>
-                                </form>
-                            }}
+                  {/*TODO: Сделать маску для номера телефона (для разных стран) */}
+                  <Field
+                    name="phone"
+                    validate={composeValidators(
+                      required(),
+                      fullTelNumber(18)
+                    )}
+                    render={({ input, meta }) => {
+                      return <div className={
+                        meta.error && meta.touched
+                          ? "b-input error"
+                          : "b-input"}
+                      >
+                        <InputMask
+                          className="b-input__input"
+                          id="phone"
+                          mask="+7 (999) 999-99-99"
+                          maskChar={null}
+                          placeholder="Ваш номер телефона"
+                          {...input}
                         />
-                        
-                        <div className="b-registration__sub-navigation">
-                            <span className="b-registration__sub-question"> У вас уже есть профиль? </span>
-                            <Link to={configPathRouter.authorization}>Войти</Link>
-                        </div>
-                    </div>
-                </div>
-                {preloader 
-                    ? <Preloader />
-                    : null
-                }
+                        {meta.error && meta.touched
+                          ? <div className='b-input__error'>{meta.error}</div>
+                          : null
+                        }
+                      </div>
+                    }}
+                  />
+
+                  <Field
+                    name="password"
+                    validate={required()}
+                    render={({ input, meta }) => {
+                      return <Input
+                        {...input}
+                        placeholder="Пароль"
+                        typeInput="password"
+                        idInput="password"
+                        nameInput={input.name}
+                        error={meta.error && meta.touched ? meta.error : null}
+                      />
+                    }}
+                  />
+
+                  <Field
+                    name="c_password"
+                    validate={(value, allValues) => composeValidators(
+                      required(),
+                      confirmPassword(allValues.password)
+                    )(value)
+                    }
+                    render={({ input, meta }) => {
+                      return <Input
+                        {...input}
+                        placeholder="Повторите пароль"
+                        typeInput="password"
+                        idInput="c_password"
+                        nameInput={input.name}
+                        error={meta.error && meta.touched ? meta.error : null}
+                      />
+                    }}
+                  />
+
+                  {/* { name, id, text, value, checked, modif } */}
+                  <Field
+                    name="is_trainer"
+                    type='checkbox'
+                    render={({ input }) => {
+                      return <Checkbox
+                        {...input}
+                        id="is_trainer"
+                        value="1"
+                        modif="b-checkbox--white"
+                      >
+                        <span>Я тренер</span>
+                      </Checkbox>
+                    }}
+                  />
+
+                  <div className="b-registration__button-wrapper">
+                    <Button
+                      type='submit'
+                      modif="b-button--login"
+                      name='Зарегистрироваться'
+                    />
+                  </div>
+
+                  <Field
+                    name="is_confirm-personal"
+                    type='checkbox'
+                    validate={required('Ваше согласие обязательно')}
+                    render={({ input, meta }) => {
+                      return <Checkbox
+                        {...input}
+                        id="is_confirm-personal"
+                        modif="b-checkbox--white b-checkbox--align-top b-checkbox--agreement"
+                        error={meta.error && meta.touched ? meta.error : null}
+                      >
+                        <span>Я принимаю условия <a href="/agreement" title="пользовательское соглашение" target="blank"> соглашения</a></span>
+                      </Checkbox>
+                    }}
+                  />
+                </form>
+              }}
+            />
+
+            <div className="b-registration__sub-navigation">
+              <span className="b-registration__sub-question"> У вас уже есть профиль? </span>
+              <Link to={configPathRouter.authorization}>Войти</Link>
             </div>
-        )
-    }
+          </div>
+        </div>
+        {preloader
+          ? <Preloader />
+          : null
+        }
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = ({ registration }) => {
-    return {
-        preloader: registration.preloader
-    }
+  return {
+    preloader: registration.preloader
+  }
 }
 
 export default connect(mapStateToProps)(Registration);
