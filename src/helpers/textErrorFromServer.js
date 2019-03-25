1356fcfb8bf32dd3d5378eff9c003ac2ba5b856f
vladka.dovzh@gmail.com <vladka.dovzh@gmail.com>
2019-03-25 23:39:20 +0400

    fix

--- src/helpers/textErrorFromServer.js
+++ src/helpers/textErrorFromServer.js
@@ -2,9 +2,11 @@ function textErrorFromServer(responseServer) {
     console.log(responseServer.response);
     if(responseServer.response.data.message === 'Unauthorized') {
         return 'Ошибка! Неверный номер или пароль.'
-    } 
+    } else if(responseServer.response.data.data) {
         return responseServer.response.data.data[Object.keys(responseServer.response.data.data)[0]][0]
+    }
     
+    return responseServer.response.data.message
 }
 
 export default textErrorFromServer;
