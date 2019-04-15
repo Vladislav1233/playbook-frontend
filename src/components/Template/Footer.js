cc9823acee307435269c5cd7746247285924f5d4
Amed <amedomary@mail>
2019-04-15 22:26:05 +0400

    footer fix

--- src/components/Template/Footer.js
+++ src/components/Template/Footer.js
@@ -40,14 +40,9 @@ class Footer extends Component {
             <footer className={ rootClassFooter }>
                 <div className="container">
                     <div className="b-footer__wrapper">
-                        <div className="b-footer__left">
                             {/* <a className="b-footer__link" href="mailto:manage.playbook@gmail.com" title="Написать нам">Написать нам</a> */}
                             {/* <a className="b-footer__link b-footer__link--disabled" href="" title="Помощь">Помощь</a> */}
                             <small className="b-footer__copyright">© playbook 2018-2019 • все права защищены</small>
-                        </div>
-                        {/* <div className="b-footer__center">
-                        </div> */}
-                        <div className="b-footer__right">
                             {/* <ul className="b-footer__social-list">
                                 <li className="b-footer__social-item">
                                     <i className="fab fa-google-play"></i>
@@ -56,11 +51,9 @@ class Footer extends Component {
                                     <i className="fab fa-apple"></i>
                                 </li>
                             </ul> */}
-                            <a className="b-footer__link" href="https://vk.com/playbooksu" title="PlayBook вконтакте" target="_blank">
-                                <i class="fab fa-vk"></i>
-                                <Tablet> - Мы вконтакте </Tablet>
-                            </a>
-                        </div>
+                        <a className="b-footer__social-link" href="https://vk.com/playbooksu" title="PlayBook вконтакте" target="_blank">
+                            <i class="fab fa-vk"></i>
+                        </a>
                     </div>
                 </div>
             </footer>
